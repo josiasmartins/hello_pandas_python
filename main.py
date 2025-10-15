f83af94b54c3144ae6aa1b037f7864124b03999c
Juan <juanmartinscaitano@gmail.com>
2025-10-14 23:10:28 -0300

    feature: initial project

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import pandas as pd
+
+def gerar_relatorio():
+    dados = {
+        'Nome': ['Rafa', 'Ana'],
+        'Idade': [23, 35],
+        'Cidade': ['SP', 'RJ']
+    }
+    df = pd.DataFrame(dados)
+    print('Relatório de pessoas')
+    print(df)
+
+if __name__ == "__main__": 
+    gerar_relatorio()    
